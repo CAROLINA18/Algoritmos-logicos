eba09f4706378c2ee037b659aefa425a0adad489
CAROLINA18 <ana.carolina.aristizabal@gmail.com>
2023-07-24 14:50:46 -0500

    Update diferenciadeFechas.js

--- diferenciadeFechas.js
+++ diferenciadeFechas.js
@@ -2,8 +2,8 @@ function diferenciaDeDias(fecha1 , fecha2){
    const fechaF = new Date(fecha1)
    const fechaS = new Date(fecha2)
    console.log(fechaF)
-   var difference =   fechaS.getTime() - fechaF.getTime()
+   var difference =   Math.abs(fechaS.getTime() - fechaF.getTime())
    
-   return difference/(1000 * 3600 * 24)
+   return Math.floor(difference/(1000 * 3600 * 24))
 }
 console.log(diferenciaDeDias('Dec 1, 2023', 'Dec 24, 2023'))
